4a5175b32637659a28b0647c25065181f21b6502
RassX-o7 <rassx777@gmail.com>
2026-06-11 20:26:03 +0530

    add statistical model, stable

--- main.py
+++ main.py
@@ -53,10 +53,12 @@ winLines={ (1,2,3):"0,125,750,125",
 temp = lambda x: print("temp")
 class TicTacToe:
     wins=[[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9]]
-    def __init__(self,window:tk.Tk,first_move):
+    computer_models=["Statistical","MinMax","Random","Q-Learning","Comparison"]
+    def __init__(self,window:tk.Tk,first_move,model):
         self.window=window
         self.window.resizable(False,False)
         self.window.geometry("750x790")
+        self.computer_model=model
         # self.window.focus_force()
         self.first_move=first_move
         self.curr_move=first_move
@@ -76,10 +78,13 @@ class TicTacToe:
         # self.play_again_button.pack(self.window,anchor="center",side="LEFT") # what was i thinking , winow in pack + Capital error
         self.reset_model_button.pack(side="right",anchor="center",padx=(0,270))
         # self.reset.pack(after=self.canvas)
-        # self.window.bind("<Button-1>",func=self.player_move)
+        # self.window.bind("<Button-1>",func=self.player_move) # check resource why window.bind not preffered
         self.canvas.bind("<Button-1>",func=self.player_move)
         if self.first_move=="computer":
-            window.after(1000,self.computer_move)
+            if not self.computer_model=="Statistical":
+                window.after(1000,self.computer_move)
+            else:
+                window.after(100,self.computer_move)
     @staticmethod
     def _checkerx(state,wins=wins):
         for win in wins:
@@ -102,7 +107,7 @@ class TicTacToe:
         return any(set(win).issubset(state) for win in wins)
     def play_again(self):
         self.clear_window()
-        TTT=TicTacToe(self.window,first_move=self.first_move)
+        TTT=TicTacToe(self.window,first_move=self.first_move,model=self.computer_model)
     def reset_model(self):
         self.clear_window()
         Appx_new=App(self.window)
@@ -140,14 +145,57 @@ class TicTacToe:
                 except:
                     ""
                 return
-            self.window.after(1000,self.computer_move)
+            self.window.after(300,self.computer_move)
     def computer_move(self):
+        # computer_models=["Statistical","MinMax","Random","Q-Learning","Comparison"]
         if not self.canvas.winfo_exists():
             return
-        while (random_move:=random.randint(1,9)) in self.board_state: ""
-        self.canvas.create_oval(eval(CircleCoords[random_move]),outline="white")
-        self.computer_state.append(random_move)
-        self.board_state.append(random_move)
+        if self.computer_model == self.computer_models[2]:
+            while (computer_final_move:=random.randint(1,9)) in self.board_state: ""
+        elif self.computer_model==self.computer_models[0]:
+            # parse_case_file() only once not on each move
+            left_states=list(set([1,2,3,4,5,6,7,8,9])-set(self.board_state))
+            matches(self.board_state,caseList)
+            curr_total_moves=len(self.board_state)
+            # win_dict=dict()
+            win_dict={nxt:0 for nxt in left_states}
+            # lose_dict=dict() #include or not , maximsize win_dict-lose_dict?
+            lose_dict={nxt:0 for nxt in left_states}
+            
+            for case in futureCases:
+                nxt_move=case[curr_total_moves]
+                idx=caseList.index(case)
+                if stateList[idx]=="Win" and (winnerList[idx]== ("P2" if self.first_move == "player" else "P1" if self.first_move == "computer" else "")):                           
+                    win_dict[nxt_move]=win_dict.get(nxt_move,0)+1
+                elif stateList[idx]=="Win" and (winnerList[idx]== ("P1" if self.first_move == "player" else "P2" if self.first_move == "computer" else "")):
+                    lose_dict[nxt_move]=lose_dict.get(nxt_move,0)+1
+            final_dict={nxt:win_dict[nxt]-lose_dict[nxt] for nxt in left_states}
+            # computer_final_move = max(win_dict,key=win_dict.get) # check resource .get NOT get() , exec immediately , needs atleast 1 arg given 0
+            computer_final_move = max(final_dict,key=win_dict.get)
+            print(win_dict)
+            print(lose_dict)
+            print(final_dict)
+            for nxt in left_states:
+                temp_check=self.player_state.copy()
+                temp_check.append(nxt)
+                if self._checker(temp_check):
+                    print(temp_check)
+                    print("close")
+                    computer_final_move=nxt
+                    break
+            for nxt in left_states:
+                temp_check=self.computer_state.copy()
+                temp_check.append(nxt)
+                if self._checker(temp_check):
+                    print(temp_check)
+                    print("instawin")
+                    computer_final_move=nxt
+                    break
+
+
+        self.canvas.create_oval(eval(CircleCoords[computer_final_move]),outline="white")
+        self.computer_state.append(computer_final_move)
+        self.board_state.append(computer_final_move)
         if (lose_case:=TicTacToe._checker(self.computer_state)):
             self.canvas.create_line(eval(winLines[tuple(lose_case)]),fill="red",width=10)
             # ttk.Label(self.window,text="COMPUTER WON !!").pack()
@@ -177,12 +225,13 @@ class App:
         self.computer_models=["Statistical","MinMax","Random","Q-Learning","Comparison"]
         for model in self.computer_models:
             # ttk.Button(self.window1, text=model,state= False if model!="Random" else True,command=lambda m=model: self.ttt_ui(computer=m)).pack(pady=3, fill='x', padx=20)
-            ttk.Button(self.window1, text=model,state= "disabled" if model!="Random" else "normal", command=lambda m=model: self.ttt_ui(computer=m)).pack(pady=3, fill='x', padx=20)
-            #disabled and normal NOT true/false
+            ttk.Button(self.window1, text=model,state= "disabled" if model not in ["Random","Statistical"] else "normal", command=lambda m=model: self.ttt_ui(computer=m)).pack(pady=3, fill='x', padx=20)
+            #disabled and normal, NOT true/false
     def clear_window(self): 
         for wd in self.window1.winfo_children(): # NOTE the hardcoded window1 attr , default must be winow
             wd.destroy()
     def ttt_ui(self,computer=None):
+        self.computer_model=computer
         for wiget in self.window1.winfo_children(): # OR use self.clear_winow() < custom wrapper func SAME THING
             wiget.destroy()
         self.window1.geometry("200x100")
@@ -192,32 +241,49 @@ class App:
         ttk.Button(self.window1,text="Computer",command=self.computer_first).pack(pady=4)
     def player_first(self):
         self.clear_window()
-        TTT=TicTacToe(self.window1,"player")
+        TTT=TicTacToe(self.window1,"player",self.computer_model)
         try:playsound(r"assets\initalize.mp3",block=False)
         except:""
         # window1.mainloop()
     def computer_first(self):
         # self.window1.destroy()
         self.clear_window()
-        TTT=TicTacToe(self.window1,"computer")
+        # if self.computer_model=="Statistical":
+        #     parse_case_file()
+        TTT=TicTacToe(self.window1,"computer",self.computer_model)
         try:playsound(r"assets\initalize.mp3",block=False)
         except:""
         # window3.focus_force() 
         # window3.mainloop()
-# file=open("ttt_cases/allCasesNoDumbforWinningAndLosingBoth.txt","rt")
-# allCases=file.read().splitlines()
-# caseList=[]
-# stateList=[]
-# winnerList=[]
-# futureWinCases=[]
-# for line in allCases:
-#     line.rstrip()
-#     L1=line.split(",",2)
-#     L1=[j.strip() for j in L1]
-#     caseList.append(eval(L1[2][6:])) 
-#     stateList.append(L1[0][8:])  # Win Draw
-#     winnerList.append(L1[1][9:]) # P1 P2 Nil
-# print(caseList)
+caseList=[]
+stateList=[]
+winnerList=[]
+futureWinCases=[]
+def matches(target:list,cases:list): # matches of target in cases
+    global futureCases
+    futureCases=[]
+    for case in cases:
+        if exactSubSet(case,target):
+            futureCases.append(case)
+def exactSubSet(Parent:list, Child :list): # is child exact subset of parent , starting from begining
+    for idx in range(len(Child)):
+        # print("max is ",range(len(Child)))
+        # print(Parent,Child,idx)
+        if not Child[idx] == Parent[idx]:
+            return False
+    return True
+def parse_case_file():
+    file=open("ttt_cases/allCasesNoDumbforWinningAndLosingBoth.txt","rt")
+    allCases=file.read().splitlines()
+    for line in allCases:
+        line=line.rstrip()
+        L1=line.split(",",2) # 2times split , so 3 length list [a,b] 1st split , [a,c,d] 2nd split
+        L1=[j.strip() for j in L1]
+        caseList.append(eval(L1[2][6:])) 
+        stateList.append(L1[0][8:])  # Win Draw
+        winnerList.append(L1[1][9:]) # P1 P2 Nil
+    file.close()
+parse_case_file()
 window=tk.Tk()
 # window.geometry("800x800")
 window.title("TicTacToe")
